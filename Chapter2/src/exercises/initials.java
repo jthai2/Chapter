5806aae3aaa8b98090d9fa80063ca5cde5266cff
jthai2 <jimmythai35@gmail.com>
2017-09-10 17:33:29 -0500

    update initials

---
+++ Chapter2/src/exercises/initials.java
@@ -0,0 +1,17 @@
+package exercises;
+
+public class initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char firstInitial, secondInitial, lastInitial;
+		
+		firstInitial = 'J';
+		secondInitial = 'M';
+		lastInitial = 'T';
+	
+		
+	System.out.println(firstInitial + "." + secondInitial + "." + lastInitial + ".");
+	}
+
+}
