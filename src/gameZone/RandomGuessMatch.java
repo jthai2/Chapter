6f35257a32b0b22d6462d022003ddb6e8c634fcb
jthai2 <jimmythai35@gmail.com>
2017-09-11 09:49:26 -0500

    update randomguess

--- src/gameZone/RandomGuessMatch.java
+++ src/gameZone/RandomGuessMatch.java
@@ -6,11 +6,20 @@ public class RandomGuessMatch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int difference;
 		int random;
+		int number;
 		
 		random = 1 + (int)(Math.random() * 5);
 		
-		JOptionPane.showMessageDialog(null, "Guess a number");
+		JOptionPane.showMessageDialog(null, "Guess random number between 1 and 5");
+		number = input.nextInt();
+		
+		difference = random - number;
+		
+		JOptionPane.showMessageDialog(null, "Difference of random nuumber and your guessed number is" + difference);
+		JOptionPane.showMessageDialog(null, "Random number is " + random);
+		
 		
 		
 	}
