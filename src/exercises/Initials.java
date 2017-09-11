84d8499ee5dc0cc55edffd1844f53ab332d821e6
jthai2 <jimmythai35@gmail.com>
2017-09-11 08:36:04 -0500

    update eggs

--- src/exercises/Initials.java
+++ src/exercises/Initials.java
@@ -4,7 +4,14 @@ public class Initials {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char firstInitial, secondInitial, lastInitial;
+		
+		firstInitial = 'J';
+		secondInitial = 'M';
+		lastInitial = 'T';
+	
+		
+	System.out.println(firstInitial + "." + secondInitial + "." + lastInitial + ".");
 	}
 
 }
