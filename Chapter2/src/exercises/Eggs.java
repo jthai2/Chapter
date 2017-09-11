9e5d8ea9948dd7490453f5ec9a008fbbd3503cab
jthai2 <jimmythai35@gmail.com>
2017-09-10 23:39:35 -0500

    update eggs

---
+++ Chapter2/src/exercises/Eggs.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Eggs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double dozenEggs = 3.25;
+		double singleEgg = .45;
+		int amountForDozenEggs;
+		int amountForSingleEgg;
+		int amountEntered;
+		int dozenEgg = 12;
+		double totalCost;
+		double totalForSingleEgg = 6.50;
+		double totalForDozenEggs = 1.35;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter an amount of eggs >>>");
+		amountEntered = input.nextInt();
+		
+		amountForDozenEggs =  amountEntered / dozenEgg;
+		amountForSingleEgg = amountEntered % dozenEgg;
+		totalCost = totalForDozenEggs + totalForSingleEgg;
+		totalForDozenEggs = dozenEggs * amountForDozenEggs;
+		totalForSingleEgg = singleEgg * amountForSingleEgg;
+		
+		System.out.println("You ordered " + amountEntered + " eggs. That's " + amountForDozenEggs + " dozen at $3.25 per dozen and " 
+				+ amountForSingleEgg + " loose eggs for .45 cents" + " each for a total of $" + totalCost );
+
+	}
+
+}
